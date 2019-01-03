1c91900801a4cc01579f431578c480da7c72bd66
Akira F <akiraff@hotmail.com>
2019-01-04 09:16:58 +1000

    implemented login form

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import RegisterForm from '../forms/Register/RegisterForm';
+import LoginForm from '../forms/Login/LoginForm';
 
 const user = (props) => {
     return (
         <div>
             <h1>This is register form</h1>
             <RegisterForm />
+
+            <h1>This is a login form</h1>
+            <LoginForm />
         </div>
     );
 }
---
+++ src/components/forms/Login/LoginForm.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class LoginForm extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            login: {
+                email: "",
+                password: ""
+            }
+        };
+    }
+    handleSubmitForm(e) {
+        e.preventDefault();
+        const data = this.state.login;
+        let login = {...this.state.login};
+
+        if (login.email > 0 || login.password > 0) {
+            axios.post('/login', data).then(res => console.log(res)).catch(err => console.log(err));
+
+            // Reset form value
+            login.email = ""
+            login.password = "";
+            this.setState({login});
+            setTimeout(() => window.location = '/dashboard', 2000);
+        }
+    }
+
+    handleEmailValue(e) {
+        let login = {...this.state.login};
+        login.email = e.target.value;
+
+        this.setState({login});
+    }
+
+    handlePasswordValue(e) {
+        let login = {...this.state.login};
+        login.password = e.target.value;
+
+        this.setState({login});
+    }
+
+    render() {
+        return (
+            <form>
+                <label>E-mail</label>
+                <input value={this.state.login.email} name="email" onChange={this.handleEmailValue.bind(this)} type="email" />
+
+                <label>Password</label>
+                <input value={this.state.login.password} name="password" onChange={this.handlePasswordValue.bind(this)} type="password" />
+
+                <button onClick={this.handleSubmitForm.bind(this)}>Login</button>
+            </form>
+        );
+    }
+}
+
+export default LoginForm;
